ff33ef17f95649a73ceb16ca5c57554f3d2e404a
Tucker Fredrickson <tuck.fredrickson@gmail.com>
2016-09-07 13:41:06 -0700

    added interactivity the the trash symbol

--- js/app.js
+++ js/app.js
@@ -4,8 +4,17 @@ $(document).ready(function(){
 	$(".submit-button").on("click", function(event) {
 		console.log("click");
 		var item = $(".enter-item-blank").val();
-		console.log(item);
+		console.log("item");
 		$("#items-ul").append("<li class='li-active'><span class='green-dot'></span>" + item + "<span class='trash-can fa fa-trash-o'></span></li>");
 		event.preventDefault();
+	});
+
+});
+
+$(document).ready(function() {
+	console.log("jquery2");
+	$("ul").on("click", ".fa-trash-o", function(){
+		console.log("clicked");
+		$(this).closest('li').remove();
 	});
 });
