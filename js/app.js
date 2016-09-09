10e84d92c15f7b4ce80fcdb8996829dcf5bac02c
Tucker Fredrickson <tuck.fredrickson@gmail.com>
2016-09-08 18:56:16 -0700

    added some interactivity plus

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
 		console.log("click");
 		var item = $(".enter-item-blank").val();
 		console.log("item");
-		$("#items-ul").append("<li class='li-active'><span class='green-dot'></span>" + item + "<span class='trash-can fa fa-trash-o'></span></li>");
-		event.preventDefault();
+		$("#items-ul").append("<li class='li-active'><span id='green' class='green-dot'></span>" + item + "<span class='trash-can fa fa-trash-o'></span></li>");
+			event.preventDefault();
 	});
 
 });
@@ -15,6 +15,9 @@ $(document).ready(function() {
 	console.log("jquery2");
 	$("ul").on("click", ".fa-trash-o", function(){
 		console.log("clicked");
-		$(this).closest('li').remove();
+		$(this).closest("li").addClass("li-inactive");
+		$(this).closest(".fa-trash-o").removeClass("fa-trash-o").append("<span class='fa fa-plus plus-sign'></span>");
+		$(this).closest("#green").remove();
 	});
+
 });
