2af24183048754669ca251cec290d1a18bc8f823
Tucker Fredrickson <tuck.fredrickson@gmail.com>
2016-09-12 14:00:52 -0700

    Added Plus sign functionality

--- js/app.js
+++ js/app.js
@@ -1,23 +1,37 @@
 $(document).ready(function(){
-	console.log("jquery");
-	//var item = $(".enter-item-blank").val();
 	$(".submit-button").on("click", function(event) {
 		console.log("click");
 		var item = $(".enter-item-blank").val();
 		console.log("item");
 		$("#items-ul").append("<li class='li-active'><span id='green' class='green-dot'></span>" + item + "<span class='trash-can fa fa-trash-o'></span></li>");
-			event.preventDefault();
+		event.preventDefault();
+		$(".enter-item-blank").val('Enter List Item :)');	
 	});
 
-});
+	$(".enter-item-blank").keypress(function(e) {
+		if(e.which == 13) {
+		console.log("enter");
+		var item = $(".enter-item-blank").val();
+		console.log("item");
+		$("#items-ul").append("<li class='li-active'><span id='green' class='green-dot'></span>" + item + "<span class='trash-can fa fa-trash-o'></span></li>");
+		this.value = '';
+		}
+	});
 
-$(document).ready(function() {
-	console.log("jquery2");
+	$(".enter-item-blank").on('click focusin', function() {
+		this.value = '';
+	});
+
 	$("ul").on("click", ".fa-trash-o", function(){
 		console.log("clicked");
 		$(this).closest("li").addClass("li-inactive");
-		$(this).closest(".fa-trash-o").removeClass("fa-trash-o").append("<span class='fa fa-plus plus-sign'></span>");
-		$(this).closest("#green").remove();
+		$(this).closest(".fa-trash-o").removeClass("fa-trash-o trash-can").addClass('fa fa-plus plus-sign');
+	});
+
+	$("ul").on("click", ".fa-plus", function(){
+		console.log("clicked");
+		$(this).closest("li").removeClass("li-inactive");
+		$(this).closest(".fa-plus").removeClass("fa-plus").addClass('fa fa-trash-o trash-can');
 	});
 
 });
